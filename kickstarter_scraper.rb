943ec76cbbe6530ba0fc4ff4dbbe8d982bed6814
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-26 20:02:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,9 +14,8 @@ def create_project_hash
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    }
-  end
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i}
+    end  
   end
 
   # return the projects hash
